394a4b9592172fd5ed59af0e2bd00b3a7698121c
Aleksander <aleksievisa@gmail.com>
2025-08-09 20:52:13 +0300

    v0.1 test

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,6 +1,6 @@
 // app/opengraph-image.tsx
 import { ImageResponse } from "next/og";
-
+export const runtime = "edge";
 export const alt = "MAGIC PROMPT — 19.99$ за 60 минут";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
