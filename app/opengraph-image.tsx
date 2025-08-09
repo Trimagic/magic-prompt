0ffc4ddf7f2e2da4277dc689fba9ec58d1a9d524
Aleksander <aleksievisa@gmail.com>
2025-08-09 21:05:38 +0300

    v0.1 test

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -6,7 +6,7 @@ export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
 export default function Image() {
-  const imageUrl = new URL("/og.png", process.env.APP_URL).href;
+  const imageUrl = "https://magic-prompt-3jr.pages.dev/og.png";
 
   return new ImageResponse(
     (
